30e0e6798bdade8172c9b0f4addc1b2836006832
Khánh Nguyễn <meikahn0109@gmail.com>
2026-01-26 15:29:15 +0700

    code xong doGet

--- BaiTH1_3/src/main/java/AboutMe.java
+++ BaiTH1_3/src/main/java/AboutMe.java
@@ -12,30 +12,8 @@ import java.io.IOException;
  */
 @WebServlet("/AboutMe")
 public class AboutMe extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AboutMe() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.getRequestDispatcher("/index.html").forward(request, response);
 	}
 
 }
