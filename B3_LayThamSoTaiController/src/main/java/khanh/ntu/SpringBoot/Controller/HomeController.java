d15399b4193c757f9cb1a368aa9d9b5d841e0507
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-07 11:15:24 +0700

    B3

---
+++ B3_LayThamSoTaiController/src/main/java/khanh/ntu/SpringBoot/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package khanh.ntu.SpringBoot.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String B3(@RequestParam("mssv") String mssv, @RequestParam("ten") String ten, ModelMap m)
+	{
+		m.addAttribute("name", ten);
+		m.addAttribute("id", mssv);
+		return "index2";
+	}
+}
