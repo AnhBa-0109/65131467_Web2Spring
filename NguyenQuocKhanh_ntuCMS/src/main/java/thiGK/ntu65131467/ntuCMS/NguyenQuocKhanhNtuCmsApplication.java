29fee767fc36db7a993a653db364f40cf63f85f3
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-11 14:37:14 +0700

    Bài thi thử

---
+++ NguyenQuocKhanh_ntuCMS/src/main/java/thiGK/ntu65131467/ntuCMS/NguyenQuocKhanhNtuCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65131467.ntuCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenQuocKhanhNtuCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenQuocKhanhNtuCmsApplication.class, args);
+	}
+
+}
---
+++ NguyenQuocKhanh_ntuCMS/src/test/java/thiGK/ntu65131467/ntuCMS/NguyenQuocKhanhNtuCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65131467.ntuCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenQuocKhanhNtuCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
