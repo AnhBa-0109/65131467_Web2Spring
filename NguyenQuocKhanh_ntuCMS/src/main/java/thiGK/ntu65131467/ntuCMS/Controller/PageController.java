f9f4a2b988685e93a57ca6ee9ffe43724a445d7b
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-21 08:27:55 +0700

    Thay đổi Code Controller để trang html có thể hiển thị dữ liệu từ db

--- NguyenQuocKhanh_ntuCMS/src/main/java/thiGK/ntu65131467/ntuCMS/Controller/PageController.java
+++ NguyenQuocKhanh_ntuCMS/src/main/java/thiGK/ntu65131467/ntuCMS/Controller/PageController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,22 +14,25 @@ import thiGK.ntu65131467.ntuCMS.Model.Page;
 import thiGK.ntu65131467.ntuCMS.Repository.PageRepository;
 import thiGK.ntu65131467.ntuCMS.services.PageService;
 
-@RestController
-public class PageController {
+@Controller
+public class PageController {	
 	
 	@Autowired
 	PageService pService;
 	
-	@GetMapping("restAPI/page/all")
-	public List<Page> getAllPage()
+	@GetMapping("/dashboard")
+	public String Home()
+	{
+		return "home";
+	}
+	
+	@GetMapping("/page/all")
+	public String getAllPage(ModelMap m)
 	{
 		List<Page> dsTrang = new ArrayList<Page>();
 		dsTrang = pService.getAllPage();
-		return dsTrang;
+		m.addAttribute("ListPage", dsTrang);
+		return "allPage";
 	}
 	
-	@GetMapping("restAPI/page/{id}")
-	public Page getPageById(@PathVariable("id") int id) {
-		return pService.getPageByID(id);
-	}
 }
