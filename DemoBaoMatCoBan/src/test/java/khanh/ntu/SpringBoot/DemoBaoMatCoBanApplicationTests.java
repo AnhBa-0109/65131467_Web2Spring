ce935b6be24507c9bb38be8442a635f8fc18a503
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-25 15:24:42 +0700

    B1_SpringSecurity

---
+++ DemoBaoMatCoBan/src/test/java/khanh/ntu/SpringBoot/DemoBaoMatCoBanApplicationTests.java
@@ -0,0 +1,13 @@
+package khanh.ntu.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBaoMatCoBanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
