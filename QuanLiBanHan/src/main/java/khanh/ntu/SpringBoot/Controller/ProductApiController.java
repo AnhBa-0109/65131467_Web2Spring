026221124e5d0f4975ed0fc426c07fda76397127
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-31 18:04:50 +0700

    Thêm annotation cần thiết vào SecurityConfig để sử dụng @PreAuthorize

---
+++ QuanLiBanHan/src/main/java/khanh/ntu/SpringBoot/Controller/ProductApiController.java
@@ -0,0 +1,17 @@
+package khanh.ntu.SpringBoot.Controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductApiController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('USER')")
+    public String getAllProducts() {
+        return "List of products (JWT protected)";
+    }
+}
