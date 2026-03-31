371b2b2cfd754c88bb371ecc0a1e5c067b1757bd
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-31 17:58:31 +0700

    Cập nhật code SecurityConfig

--- QuanLiBanHan/src/main/java/khanh/ntu/SpringBoot/config/SecurityConfig.java
+++ QuanLiBanHan/src/main/java/khanh/ntu/SpringBoot/config/SecurityConfig.java
@@ -1,14 +1,35 @@
 package khanh.ntu.SpringBoot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import khanh.ntu.SpringBoot.Service.CustomUserDetailsService;
+import khanh.ntu.SpringBoot.filter.JwtRequestFilter;
+import khanh.ntu.SpringBoot.security.JwtAuthenticationEntryPoint;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -16,22 +37,31 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    	AuthenticationManagerBuilder authenticationManagerBuilder = 
+    		http.getSharedObject(AuthenticationManagerBuilder.class);
+    	    
+    	authenticationManagerBuilder
+    		.userDetailsService(userDetailsService)
+    		.passwordEncoder(passwordEncoder());
+    	        
+    	return authenticationManagerBuilder.build();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
+            .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(unauthorizedHandler))
+            .sessionManagement(sessionManagent -> sessionManagent.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/register", "/css/**", "/js/**").permitAll()
-                .requestMatchers("/products/new", "/products/edit/**", "/products/delete/**").hasRole("ADMIN")
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/products/**").hasRole("USER")
                 .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/products", true)
-            )
-            .logout(logout -> logout
-                .permitAll()
-                .logoutSuccessUrl("/login?logout")
             );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
