4a8925ed4a7fccd1bd8ccccca771f272f6282bb6
Khánh Nguyễn <meikahn0109@gmail.com>
2026-03-31 18:22:40 +0700

    Đổi độ dài khóa từ 512bits thành 256bits, thêm getter setter cho Response

--- QuanLiBanHan/src/main/java/khanh/ntu/SpringBoot/Model/JwtResponse.java
+++ QuanLiBanHan/src/main/java/khanh/ntu/SpringBoot/Model/JwtResponse.java
@@ -14,4 +14,30 @@ public class JwtResponse {
         this.token = token;
         this.username = username;
     }
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+    
+    
 }
