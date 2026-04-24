350b881ee23e9fcf105735d552bb198ae91d02cb
Khánh Nguyễn <meikahn0109@gmail.com>
2026-04-24 14:17:22 +0700

    lần thứ 5

--- BaiThiGiuaKi2/src/main/java/thigk2/NguyenQuocKhanh/controllers/HomeController.java
+++ BaiThiGiuaKi2/src/main/java/thigk2/NguyenQuocKhanh/controllers/HomeController.java
@@ -1,5 +1,24 @@
 package thigk2.NguyenQuocKhanh.controllers;
 
-public class HomeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import thigk2.NguyenQuocKhanh.services.TinService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	TinService tinService;
+	
+	
+	@GetMapping("/home")
+	public String home(ModelMap m)
+	{
+		m.addAttribute("listTheLoai", tinService.getAllTheLoai());
+		m.addAttribute("listTin", tinService.getAllTin());
+		return "home";
+	}
 }
---
+++ BaiThiGiuaKi2/src/main/java/thigk2/NguyenQuocKhanh/services/TinService.java
@@ -0,0 +1,36 @@
+package thigk2.NguyenQuocKhanh.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import thigk2.NguyenQuocKhanh.models.TheLoaiTin;
+import thigk2.NguyenQuocKhanh.models.Tin;
+import thigk2.NguyenQuocKhanh.repos.TheLoaiTinRepository;
+import thigk2.NguyenQuocKhanh.repos.TinRepository;
+
+@Service
+public class TinService {
+	
+	@Autowired
+	TinRepository tinRepo;
+	
+	@Autowired
+	TheLoaiTinRepository theloaiRepo;
+	
+	public List<Tin> getAllTin()
+	{
+		return tinRepo.findAll();
+	}
+	
+	public List<TheLoaiTin> getAllTheLoai()
+	{
+		return theloaiRepo.findAll();
+	}
+	
+	public Tin getById(int id)
+	{
+		return tinRepo.getReferenceById(id);
+	}
+}
