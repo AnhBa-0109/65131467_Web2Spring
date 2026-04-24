52c087b4a7c52ac1d2c00d1214cbe0ec1e978dec
Khánh Nguyễn <meikahn0109@gmail.com>
2026-04-24 13:49:01 +0700

    lần thứ 1

---
+++ BaiThiGiuaKi2/src/main/java/thigk2/NguyenQuocKhanh/BaiThiGiuaKi2Application.java
@@ -0,0 +1,13 @@
+package thigk2.NguyenQuocKhanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiThiGiuaKi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiThiGiuaKi2Application.class, args);
+	}
+
+}
