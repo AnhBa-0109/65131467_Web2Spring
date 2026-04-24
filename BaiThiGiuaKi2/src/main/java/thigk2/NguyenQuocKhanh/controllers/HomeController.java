f1a2d4cae0f1432fba1c64babb0e2bbff589f0e5
Khánh Nguyễn <meikahn0109@gmail.com>
2026-04-24 14:38:06 +0700

    lần thứ 8

--- BaiThiGiuaKi2/src/main/java/thigk2/NguyenQuocKhanh/controllers/HomeController.java
+++ BaiThiGiuaKi2/src/main/java/thigk2/NguyenQuocKhanh/controllers/HomeController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
 import thigk2.NguyenQuocKhanh.services.TinService;
 
 @Controller
